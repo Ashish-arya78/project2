d52595e95eddc0497d46c9f51f0da7a799472ede
Ashish-arya78 <108650312+Ashish-arya78@users.noreply.github.com>
2023-05-11 20:03:47 +0530

    about.php

--- about.php
+++ about.php
@@ -262,7 +262,7 @@
 			<div class="row">
 				<div class="col-md-12 col-sm-12 col-xs-12">
 					<div class="site-info">
-					<p>Copyright © GPCK GROUP. Created By <a href="#"></a></p>
+						<p>Copyright © GPCK GROUP. Created By <a href="#"></a></p>
 				</div>
 				</div>
 			</div>
